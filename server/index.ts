021ff76fc69916f3afd3f892eb846597a9da2259
Max Moreno <rodney.moreno@outlook.com>
2026-01-05 15:01:35 -0800

    Add new game creation endpoint

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,21 @@
 import express from "express";
 import { createServer } from "node:http";
 import { Server, Socket } from "socket.io";
-import { GameState } from "./types.js";
+import { GameState, Edge } from "./types.js";
+import { initGraph } from "./graph.js";
+import { nanoid } from "nanoid";
 const app = express();
 app.use(express.json());
 const server = createServer(app);
 const PORT = 3000;
+
+const generateInitialGameState = (): GameState => {
+  const newGraph = initGraph();
+  const newLinks: Edge[] = [];
+  const initialState = { graph: newGraph, player: undefined, links: newLinks };
+
+  return initialState;
+};
 
 const gameManager = new Map<string, string[]>();
 interface ClientToServerEvents {
@@ -14,7 +24,7 @@ interface ClientToServerEvents {
     gameId: string;
     gameState: GameState;
   }) => void;
-  "end-turn": (payload: { player: number }) => void;
+  "end-turn": (payload: { player: number; gameState: GameState }) => void;
 }
 
 interface ServerToClientEvents {
@@ -29,16 +39,23 @@ const socketManager = new Map<
 
 const gameStateManager = new Map<string, GameState[]>();
 
+app.get("/", (_req, res) => {
+  res.send("Pong");
+});
+
+app.post("/creategame", (req, res) => {
+  const newGameState = generateInitialGameState();
+  const gameId = nanoid(8);
+  gameStateManager.set(gameId, [newGameState]);
+  res.send(gameId);
+});
+
 const io = new Server<ClientToServerEvents, ServerToClientEvents>(server, {
   cors: {
     origin: "http://localhost:5174",
     methods: ["GET", "POST"],
     credentials: true,
   },
-});
-
-app.get("/", (_req, res) => {
-  res.send("Pong");
 });
 
 io.on("connection", (socket) => {
@@ -62,10 +79,13 @@ io.on("connection", (socket) => {
     });
   });
 
-  socket.on("end-turn", ({ player }) => {
+  socket.on("end-turn", ({ player, gameState }) => {
     const opponentId = gameManager.get("123456")![player];
     const opponentSocket = socketManager.get(opponentId);
-    opponentSocket!.emit("ended-turn", { player: player });
+    opponentSocket!.emit("ended-turn", {
+      player: player,
+      gameState: gameState,
+    });
   });
 });
 
